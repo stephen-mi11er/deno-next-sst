bbb81f7bb026768d3826baa78f5407ea50044737
Stephen Miller <stephen.alan.miller@gmail.com>
2024-12-21 14:56:45 -0600

    Add .gitignore and configure open-next for Deno runtime

---
+++ packages/web/frontend/open-next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    default: {
+        runtime: 'deno',
+    }
+};
+
+export default nextConfig;
